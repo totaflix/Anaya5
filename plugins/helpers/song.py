62775fc5ebe464309531584c4e2327be5f9b638f
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-01-24 10:51:10 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**Jᴏɪɴ ›› [ ᴄʜᴀɴɴᴇʟ ](https://t.me/hbbotz)**\n**Pᴏᴡᴇʀᴇᴅ Bʏ ›› [Hʙ Tᴇᴀᴍ](https://t.me/hbbotz)'
+        rep = '**Jᴏɪɴ ›› [ ᴄʜᴀɴɴᴇʟ ](https://t.me/TamilBots)**\n**Pᴏᴡᴇʀᴇᴅ Bʏ ›› [ᴛᴀᴍɪʟʙᴏᴛ](https://t.me/TamilBotz)'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
