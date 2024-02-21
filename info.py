eb98ff3918433240e496758049d44065713d92c6
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-02-21 13:45:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/MovieDiscussion24x7')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/TamilMovies4k')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/TamilMovieChat/26')
+TUTORIAL = environ.get('TUTORIAL', 'https://youtu.be/tzkMzFGZqfU?si=yTeJBTlqgV332plE')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001626468777'))
@@ -90,44 +90,35 @@ SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "seas
 
 
 # Online Stream and Download
-#NO_PORT = bool(environ.get('NO_PORT', False))
-#APP_NAME = None
-#if 'DYNO' in environ:
-#    ON_HEROKU = True
-#    APP_NAME = environ.get('APP_NAME')
-#else:
-#    ON_HEROKU = False
-#BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-#FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-#URL = "https://anayafilterbot-tamilbots.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-#    "https://anayafilterbot-tamilbots.koyeb.app/".format(FQDN, PORT)
-#SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-#WORKERS = int(environ.get('WORKERS', '4'))
-#SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
-#MULTI_CLIENT = False
-#name = str(environ.get('name', 'LazyPrincess'))
-#PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-#if 'DYNO' in environ:
-#    ON_HEROKU = True
-#    APP_NAME = str(getenv('APP_NAME'))
+NO_PORT = bool(environ.get('NO_PORT', False))
+APP_NAME = None
+if 'DYNO' in environ:
+    ON_HEROKU = True
+    APP_NAME = environ.get('APP_NAME')
+else:
+    ON_HEROKU = False
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+URL = "https://anayafilterbot-tamilbots.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://anayafilterbot-tamilbots.koyeb.app/".format(FQDN, PORT)
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+WORKERS = int(environ.get('WORKERS', '4'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
+MULTI_CLIENT = False
+name = str(environ.get('name', 'LazyPrincess'))
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+if 'DYNO' in environ:
+    ON_HEROKU = True
+    APP_NAME = str(getenv('APP_NAME'))
 
-#else:
-#    ON_HEROKU = False
-#HAS_SSL=bool(getenv('HAS_SSL',False))
-#if HAS_SSL:
-#    URL = "https://anayafilterbot-tamilbots.koyeb.app/".format(FQDN)
-#else:
-#    URL = "https://anayafilterbot-tamilbots.koyeb.app/".format(FQDN)
+else:
+    ON_HEROKU = False
+HAS_SSL=bool(getenv('HAS_SSL',False))
+if HAS_SSL:
+    URL = "https://anayafilterbot-tamilbots.koyeb.app/".format(FQDN)
+else:
+    URL = "https://anayafilterbot-tamilbots.koyeb.app/".format(FQDN)
 
-PORT = int(environ.get("PORT", 8080))
-BIND_ADDRESS = str(environ.get("BIND_ADDRESS", "0.0.0.0"))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
-HAS_SSL = str(environ.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
-NO_PORT = str(environ.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
-FQDN = str(environ.get("FQDN", BIND_ADDRESS))
-URL = "http{}://{}{}/".format(
-    "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
-)
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
