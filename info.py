a97f0ea200be0b31ce4dc2aa672025834c42cc56
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-02-21 10:38:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "seas
 
 
 
- Online Stream and Download
+# Online Stream and Download
 NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
