74098d65d1973f2c0b30889d03b492c543c05fb6
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-02-22 15:37:34 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -11,7 +11,7 @@ class Var(object):
     name = str(getenv('name', 'tgmovieprobot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '--1001626468777'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001626468777'))
     PORT = int(getenv('PORT', 8000))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
